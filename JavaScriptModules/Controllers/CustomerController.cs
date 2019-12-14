edf173edb8328abb044dfae9bee53fe6ba30e8d5
SamanthaNeilen <33734383+SamanthaNeilen@users.noreply.github.com>
2019-12-14 06:55:30 +0100

    Customer overview - embedded javascript

---
+++ JavaScriptModules/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using JavaScriptModules.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace JavaScriptModules.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            var customerList = new List<Customer>();
+
+            for (int index = 0; index < 100; index++)
+            {
+                customerList.Add(CreateCustomerFromNumer(index));
+            }
+
+            return customerList;
+        }
+
+        private Customer CreateCustomerFromNumer(int index)
+        {
+            return new Customer
+            {
+                Name = $"Customer {Guid.NewGuid()}",
+                Email = $"info@customer{index}.com",
+                Country = "Netherlands"
+            };
+        }
+    }
+}
